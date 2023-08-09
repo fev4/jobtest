d6c561f2cb7fed9b0fcaa131197290f540f08aa1
Felipe <main@feva.dev>
2023-08-09 17:30:29 -0500

    rename component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { provide, readonly } from 'vue';
-import TestOptions from './components/TestOptions.vue';
+import ListGroup from './components/ListGroup.vue';
 import * as rsanalytics from 'rudder-sdk-js';
 
 rsanalytics.load("2TiqCH4Nq2ylPFjS6UPnSK51EQp", "https://aleeasjobwesgl.dataplane.rudderstack.com");
@@ -10,7 +10,7 @@ provide('rsanalytics', readonly(rsanalytics));
 </script>
 
 <template>
-  <TestOptions />
+  <ListGroup />
 </template>
 
 <style scoped></style>
